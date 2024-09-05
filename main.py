148d7cef1fb0454eaabf1b836d4673b32a0b3abf
Anubhav099 <ait.anubhav@gmail.com>
2024-09-05 18:05:11 +0530

    commit 12

--- main.py
+++ main.py
@@ -25,16 +25,6 @@ def format_timestamp(input_timestamp):
     return utc_time.strftime(f'{day}{suffix} %B %Y - %I:%M %p UTC')
 
 
-class PushObj:
-    def __init__(self, author_name, branch, timestamp):
-        self.author_name = author_name
-        self.branch = branch
-        self.timestamp = timestamp
-        print(f"PushObj created: {self}")
-
-    def __str__(self):
-        return f'"{self.author_name}" pushed to "{self.branch}" on {self.timestamp}'
-
 # 2
 @app.post("/")
 async def read_root(request: Request):
@@ -42,31 +32,28 @@ async def read_root(request: Request):
         info = await request.json()
         print(f"{info = }")
 
-        branch_name = info['ref'].split('/')[-1]
-        push_time = info['repository']['pushed_at']
-        if isinstance(push_time, str) and push_time[-1] == 'Z':
-            print(f"str {push_time = }")
-        elif isinstance(push_time, int):
-            print(f"int {push_time = }")
-        push_time = format_timestamp(push_time)
-        print(f"converted {push_time = }")
-        print(push_time)
+        if 'pull_request' in info: # 'pull_request' event
+            """
+                Format: {author} submitted a pull request from {from_branch} to {to_branch} on {timestamp}
+                Sample: "Travis" submitted a pull request from "staging" to "master" on 1st April 2021 - 9:00 AM UTC
+            """
+            author_name = info['pull_request']['user']['login']
+            from_branch = info['pull_request']['head']['ref']
+            to_branch = info['pull_request']['base']['ref']
+            action_time = info['pull_request']['created_at']
 
+            print(f'"{author}" submitted a pull request from "{from_branch}" to "{to_branch}" on {timestamp}')
+        else: # 'push' event
+            """
+                Format: {author} pushed to {to_branch} on {timestamp}
+                Sample: "Travis" pushed to "staging" on 1st April 2021 - 9:30 PM UTC
+            """
+            branch_name = info['ref'].split('/')[-1]
+            push_time = format_timestamp(info['repository']['pushed_at'])
 
-        if 'commits' in info:
             for commit in info['commits']:
-                print(f"{commit['author']['name'] = }")
-                PushObj(commit['author']['name'], branch_name, push_time)
+                print(f'"{self.author_name}" pushed to "{self.branch}" on {self.timestamp}')
                 
-
-        # new_commit_id = info['after'] if 'after' in info else None
-
-        # repo_url = info['repository']['html_url']
-        # if 'head_commit' in info:
-            # commit_message = info['head_commit']['message']
-            # commit_url = info['head_commit']['url']
-            # commit_author = info['head_commit']['author']['username']
-
         return info
 
 
