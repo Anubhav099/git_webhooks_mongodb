89f4265c61c7e42e6f87b6b88a7d9472143ee973
Anubhav099 <ait.anubhav@gmail.com>
2024-09-05 16:47:00 +0530

    Commit 7

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ app = FastAPI()
 
 # 2
 @app.post("/")
-def read_root(request: Request):
+async def read_root(request: Request):
     print(f"{request = }")
     print(f"{request.method = }")
     print(f"{request.headers = }")
     print(f"{request.headers['x-github-delivery']} = ")
     print(f"{request.headers['x-github-event']} = ")
     print(f"{request.headers['x-github-hook-installation-target-type'] = }")
-    print(f"#{request.body() = }")
+    print(f"#{await request.body() = }")
     print(f"#{request.body = }")
     if request.headers['Content-Type'] == 'application/json':
         print(f"#{request.json = }")
-        print(f"#{request.json() = }")
-        info = json.dumps(request.json())
+        print(f"#{await request.json() = }")
+        info = json.dumps(await request.json())
         print(info)
         return info
 
