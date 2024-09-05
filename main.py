84b1d761bc8680e6490ee4e51ee0833e87e126d1
Anubhav099 <ait.anubhav@gmail.com>
2024-09-05 16:43:41 +0530

    Commit 4

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ def read_root(request: Request):
     print(f"{request.headers['x-github-delivery']} = ")
     print(f"{request.headers['x-github-event']} = ")
     print(f"{request.headers['x-github-hook-installation-target-type'] = }")
+    print(f"#{request.body() = }")
+    print(f"#{request.body = }")
     if request.headers['Content-Type'] == 'application/json':
-        info = json.dumps(request.json)
+        info = json.dumps(request.json())
         print(info)
         return info
 
